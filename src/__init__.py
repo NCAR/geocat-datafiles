c4246840bb8e364d20a11c3e3c1d7d052b35fa14
Kevin Hallock <hallock@ucar.edu>
2020-01-27 15:30:26 -0700

    Move pooch registry updating functionality into a function

--- src/__init__.py
+++ src/__init__.py
@@ -12,13 +12,22 @@ POOCH = pooch.create(
     version_dev='master',
     registry={'registry.txt': None})
 
+def _update_registry(p):
+    """
+    Updates pooch registry.txt
+    """
+    old_sha256 = p.registry["registry.txt"]
+    new_sha256 = hashlib.sha256(requests.get(p.get_url("registry.txt")).content).hexdigest()
+    if old_sha256 != new_sha256:
+        p.registry["registry.txt"] = new_sha256
+        p.load_registry(p.fetch("registry.txt"))
+
 dev_data_path = os.environ.get('GEOCAT_DATA_DIR')
 
 if dev_data_path is not None and os.path.exists(dev_data_path):
     POOCH.path = dev_data_path
 
-POOCH.registry["registry.txt"] = hashlib.sha256(requests.get(POOCH.get_url("registry.txt")).content).hexdigest()
-POOCH.load_registry(POOCH.fetch('registry.txt'))
+_update_registry(POOCH)
 
 def get(fname):
     return POOCH.fetch(fname)
